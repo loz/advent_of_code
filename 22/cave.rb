509c8f8516b9fbf0a6d5b655ed71a441a73e0d81
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2019-01-01 17:41:20 +0000

    Day 22 - Rewritten to be JIT generated

--- 22/cave.rb
+++ 22/cave.rb
@@ -3,44 +3,23 @@ class Cave
   def initialize(depth, target)
     @depth = depth
     @target = target
-    width, height = target
-    @geo_index = Array.new(height+1) { Array.new(width+1) }
-    @erosion_index = Array.new(height+1) { Array.new(width+1) }
-
-    (width+1).times do |x|
-      geo = @geo_index[0][x] = (x) * 16807
-      erosion = (geo + @depth) % 20183
-      @erosion_index[0][x] = erosion
-    end
-
-    (height+1).times do |y|
-      geo = @geo_index[y][0] = (y) * 48271
-      erosion = (geo + @depth) % 20183
-      @erosion_index[y][0] = erosion
-    end
-
-    height.times do |y|
-      yy = y + 1
-      width.times do |x|
-        xx = x + 1
-        #puts "GEO for #{xx},#{yy}"
-        e1 = @erosion_index[yy][xx-1]
-        e2 = @erosion_index[yy-1][xx]
-        geo = @geo_index[yy][xx] = e1 * e2
-        erosion = (geo + @depth) % 20183
-        @erosion_index[yy][xx] = erosion
-      end
-    end
-
-    @geo_index[height][width] = 0 #Target
-    @erosion_index[height][width] = @depth #Target
+    @width, @height = target
   end
   
   def geo_index(x,y)
-    @geo_index[y][x]
+    return 0 if x == @width && y == @height
+    if x == 0
+      return y * 48271
+    elsif y == 0
+      return x * 16807
+    else
+      e1 = erosion_level(x-1,y)
+      e2 = erosion_level(x,y-1)
+      return e1 * e2
+    end
   end
 
   def erosion_level(x,y)
-    @erosion_index[y][x]
+    return (geo_index(x,y) + @depth) % 20183
   end
 end
