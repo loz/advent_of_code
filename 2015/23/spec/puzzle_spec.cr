8fbbedd38ed2910397bbac5f012a0c8861da6734
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2019-12-08 09:51:25 +0000

    Solves 2015-23

--- 2015/23/spec/puzzle_spec.cr
+++ 2015/23/spec/puzzle_spec.cr
@@ -8,6 +8,8 @@ describe Puzzle do
     puzzle.set_register("a", 10)
     puzzle.process("hlf a")
 
+    puzzle.execute
+
     puzzle.get_register("a").should eq 5
   end
 
@@ -16,12 +18,16 @@ describe Puzzle do
     puzzle.set_register("a", 10)
     puzzle.process("tpl a")
 
+    puzzle.execute
+
     puzzle.get_register("a").should eq 30
   end
 
   it "supports inc register command" do
     puzzle = Puzzle.new
     puzzle.process("inc a")
+
+    puzzle.execute
 
     puzzle.get_register("a").should eq 1
   end
@@ -35,6 +41,41 @@ describe Puzzle do
     inc a
     EOF
 
+    puzzle.execute
+
     puzzle.get_register("a").should eq 2
+  end
+
+  it "supports jie command" do
+    puzzle = Puzzle.new
+    puzzle.process <<-EOF
+    inc a
+    jie a, +2
+    inc a
+    jie a, +2
+    inc a
+    inc a
+    EOF
+
+    puzzle.execute
+
+    puzzle.get_register("a").should eq 3
+  end
+
+  it "supports jio command" do
+    puzzle = Puzzle.new
+    puzzle.process <<-EOF
+    inc a
+    jio a, +3
+    inc a
+    inc a
+    inc a
+    jio a, -1
+    inc a
+    EOF
+
+    puzzle.execute
+
+    puzzle.get_register("a").should eq 3
   end
 end
--- 2015/23/src/puzzle.cr
+++ 2015/23/src/puzzle.cr
@@ -1,25 +1,64 @@
 class Puzzle
 
   @registers = {"a" => 0, "b" => 0} 
+  @instructions = [] of String
+  @ipointer = 0
 
   def process(str)
-    process_line(str)
+    str.each_line do |line|
+      @instructions << line
+    end
+  end
+
+  def execute
+    while @ipointer < @instructions.size
+      instruction = @instructions[@ipointer]
+      process_line(instruction)
+    end
   end
 
   def process_line(line)
-    parts = line.match(/(.+) (.+)(, .+)*/)
+    parts = line.match(/([^\s]+) ([^,]+)(, (.+))*/)
     if parts
       instruction = parts[1]
       case instruction
         when "hlf"
           reg = parts[2]
           @registers[reg] = @registers[reg].tdiv 2
+          @ipointer += 1
         when "tpl"
           reg = parts[2]
           @registers[reg] = @registers[reg] * 3
+          @ipointer += 1
         when "inc"
           reg = parts[2]
           @registers[reg] += 1
+          @ipointer += 1
+        when "jmp"
+          delta = parts[2].to_i
+          @ipointer += delta
+        when "jie"
+          reg = parts[2]
+          val = @registers[reg]
+          if val % 2 == 0
+            offset = parts[4].to_i
+            @ipointer += offset
+          else
+            @ipointer += 1
+          end
+        when "jio"
+          reg = parts[2]
+          val = @registers[reg]
+          if val == 1
+            offset = parts[4].to_i
+            @ipointer += offset
+          else
+            @ipointer += 1
+          end
+        else
+          p parts
+          puts "Unkown Instruction #{instruction}"
+          @ipointer += 1
       end
     end
   end
@@ -33,6 +72,9 @@ class Puzzle
   end
 
   def result
+    @registers["a"] = 1
+    execute
+    puts @registers
   end
 
 end
