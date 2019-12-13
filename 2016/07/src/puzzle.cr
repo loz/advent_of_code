5d58d414e2e25eefbb05c79cd7253fc7e1b0dd00
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2019-12-13 09:49:35 +0000

    Solves 2016-07

--- 2016/07/src/puzzle.cr
+++ 2016/07/src/puzzle.cr
@@ -5,7 +5,12 @@ class Puzzle
   def process(str)
     matches = [] of String
     str.each_line do |line|
-      matches << line if tls?(line)
+      if ssl?(line) 
+        puts "T: #{line}"
+        matches << line
+      else
+        puts "F: #{line}"
+      end
     end
     @matches = matches
   end
@@ -14,23 +19,71 @@ class Puzzle
     p @matches.size
   end
 
-  def tls?(str)
-    matches = str.match /(\w)(\w)\2\1/
-    if matches
-      badmatch = str.match /\[(\w)(\w)\2\1\]/
-      if badmatch
-        false
+  def mask_hype(str)
+    masked = ""
+    masking = false
+    hype = ""
+    hypes = [] of String
+    str.each_char do |ch|
+      if ch == '['
+        masking = true
+        masked += '['
+      elsif ch == ']'
+        masking = false
+        masked += "]"
+        hypes << hype
+        hype = ""
+      elsif masking
+        masked += "*"
+        hype += ch
       else
-        badmatch = matches[0].match /(\w)\1\1\1/
-        if badmatch
-          #Search rest of string
-          tls?(matches.post_match)
+        masked += ch
+      end
+    end
+    {masked,hypes}
+  end
+
+  def ssl?(str, ohypes = nil)
+    nohype, hypes = mask_hype(str)
+    hypes = ohypes if ohypes
+    match = nohype.match /(\w)(\w)\1/
+    if match
+      if match[1] != match[2]
+        bab = match[2] + match[1] + match[2]
+        babmatch = hypes.any? {|h| h.match /#{bab}/}
+        if babmatch
+          print "BAB -> #{bab}:> "
+          return true
         else
-          true
+          #Look Elsewhere in the string
+          return ssl?(match[2] + match[1] + match.post_match, hypes)
         end
+      else
+        return ssl?(match[2] + match[1] + match.post_match, hypes)
       end
     else
-      false
+      return false
+    end
+  end
+
+  def tls?(str)
+    badmatch = str.match /\[[^\]]*(\w)(\w)\2\1[^\]]*\]/
+    if badmatch && badmatch[1] != badmatch[2]
+      #print "[ABBA] -> "
+      return false
+    else
+      matches = str.match /(\w)(\w)\2\1/
+      if matches
+        if matches[1] != matches[2]
+          #print  "ABBA: #{matches[0]}"
+          return true
+        else
+          return tls?(matches[2] + matches.post_match)
+        end
+      else
+        #print "NONE -> "
+        return false
+      end
     end
   end
 
