b0ba709d7058126f75033d20997ff87266c90806
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2019-12-14 20:12:15 +0000

    WIP 2019-14, finding cheaper, but still too inefficient algorithm

--- 2019/14/src/puzzle.cr
+++ 2019/14/src/puzzle.cr
@@ -31,7 +31,9 @@ class Puzzle
     refine_r(ingredients)
   end
 
-  def refine_r(ingredients)
+  def refine_r(ingredients, best = 999_999_999)
+    return {"ORE" => best} if ingredients["ORE"]? && ingredients["ORE"] > best
+    return ingredients if only_ore?(ingredients)
     new_ingredients = {} of String => Int32
     ingredients.each do |name,count|
       if name == "ORE"
@@ -41,22 +43,76 @@ class Puzzle
         new_ingredients = merge_ingredients(new_ingredients, apply_recipe(recipe, count, name))
       end
     end
+    return new_ingredients if only_ore?(new_ingredients)
     if ingredients == new_ingredients
-      new_ingredients = refine_remainder(ingredients)
+      #new_ingredients = refine_remainder(ingredients)
+      return explore_refinements(ingredients, best)
     end
-    if ingredients == new_ingredients
-      raise "Unable To Refine!"
+    #puts "GEN :> #{new_ingredients}"
+    return refine_r(new_ingredients, best)
+  end
+
+  def explore_refinements(ingredients, best)
+    #puts "EXPLORE REFINE OF : #{ingredients}"
+    ingredients.each do |ing,have|
+        next if ing == "ORE"
+        possible = ingredients.dup
+        recipe = @recipes[ing]
+        needed, gives = recipe
+        if gives.first[0] == "ORE" 
+          takes = gives.first[1]
+          #Possible option is to refine into ORE
+          possible["ORE"] = 0 unless possible["ORE"]?
+          possible.delete ing
+          if have % needed == 0
+            possible["ORE"] += (have//needed) * takes
+          else #Round Up
+            rounded = (have//needed) + 1
+            #p "#{(have/needed)} x #{takes} ORE (->#{needed}) -> #{have} #{ing} -> #{rounded}"
+            possible["ORE"] += rounded * takes
+          end
+        else
+          #Explore other refinement
+          #puts "EXPLORE OTHER"
+          #puts "I:#{ing}x#{have} -> #{needed} => #{gives}"
+          #??? add in remainder ??
+          possible.delete ing
+          possible = merge_ingredients(possible, gives)
+          #p possible
+          #sleep 1
+        end
+        #puts "POSS: #{possible}"
+        #puts "=" * 50
+        if only_ore?(possible) 
+          result = possible
+        else
+          result = refine_r(possible, best)
+        end
+        #puts "=" * 50
+        #p result
+        #puts "=" * 50
+        total = result["ORE"]
+        #raise "Result"
+        if total < best
+          puts "NEW BEST:> #{total}"
+          best = total
+        end
     end
-    if only_ore?(new_ingredients)
-      return new_ingredients
-    else
-      puts "GEN :> #{new_ingredients}"
-      return refine_r(new_ingredients)
-    end
+    {"ORE" => best}
   end
 
   def refine_remainder(ingredients)
     #puts "REFINING REMAINEDER"
+    new_ingredients = {} of String => Int32
+    ingredients.each do |name,count|
+      if name == "ORE"
+        new_ingredients = merge_ingredients(new_ingredients, {"ORE" => count})
+      else
+        recipe = @recipes[name]
+        new_ingredients = merge_ingredients(new_ingredients, apply_recipe(recipe, count, name, true))
+      end
+    end
+    ingredients = new_ingredients
     new_ingredients = {} of String => Int32
     ingredients.each do |k, v|
       if @recipes[k]?
@@ -85,7 +141,7 @@ class Puzzle
     ingredients.all? {|k,v| k == "ORE" || v == 0 }
   end
 
-  def apply_recipe(recipe, remain, name)
+  def apply_recipe(recipe, remain, name, allowore = false)
     produced = {} of String => Int32
     #puts "Apply: #{recipe} to #{remain} x #{name}"
     consume, produce = recipe
@@ -93,6 +149,7 @@ class Puzzle
       #print ">"
       produce.each do |compound|
         comp, compn = compound
+        return {name => remain} if !allowore && comp == "ORE" #don't breakdown ORE until end
         if produced[comp]?
           produced[comp] += compn
         else
@@ -104,7 +161,7 @@ class Puzzle
     if remain > 0
       produced[name] = remain
     end
-    #print "> #{produced}"
+    #puts "> #{produced}"
     produced
   end
 
