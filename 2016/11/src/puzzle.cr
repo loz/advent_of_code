97900922c49315ab0c56c4fc04a58fd23762afbd
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2019-12-15 23:09:58 +0000

    WIP 2016-11, correct state representation and changes, only up/down floor

--- 2016/11/src/puzzle.cr
+++ 2016/11/src/puzzle.cr
@@ -33,18 +33,35 @@ class Puzzle
     valid?(empty, elevator + floor)
   end
 
-  def generate_lifts(floor)
-    options = Set.new([] of Set(Tuple(Symbol, Symbol)))
-    floor.each do |item|
+  def generate_lifts(floor,current)
+    #options = [] of Tuple(Set.new([] of Set(Item)),Set.new([] of Set(Item)))
+    options = [] of Tuple(Floor,Floor)
+
+    empty = Set.new([] of Item)
+
+    allfloor = floor + current
+    
+    #Empty Lift
+    options << {empty,current}
+
+    allfloor.each do |item|
       #just this in the lift
       option = Set{item}
-      options << option if lift_can_move?(option, floor - option)
+      ofloor = floor - option
+      if lift_can_move?(option, ofloor)
+        left = current - ofloor
+        options << {option, left}
+      end
 
       #this plus another item
       floor.each do |otheritem|
         next if item == otheritem
         option = Set{item, otheritem}
-        options << option if lift_can_move?(option, floor - option)
+        ofloor = floor - option
+        if lift_can_move?(option, ofloor)
+          left = current - ofloor
+          options << {option, left}
+        end
       end
     end
     options
@@ -80,36 +97,61 @@ class Puzzle
     set.all? {|i| i[1] == :generator }
   end
 
+
+  alias Item = Tuple(Symbol,Symbol)
+  alias Floor = Set(Item)
+  alias State = Tuple(Int32,Set(Item),Array(Set(Item)))
+
   def next_states(states, visited)
-    newstates = [] of Array(Set(Tuple(Symbol,Symbol)))
+    newstates = [] of State
     #We are visiting
     states.each { |s| visited[s] = true }
-    generated = {} of Array(Set(Tuple(Symbol,Symbol))) => Bool
+    generated = {} of State => Bool
 
     states.each do |state|
+      curfloor, lift, floors = state
+      #p curfloor
+      floor = floors[curfloor]
+      #puts floor
 
-      state.each do |floor|
-        #puts floor
-        options = generate_lifts(floor)
-        options.each do |option|
-          #puts "->#{option}"
-          newstate = [] of Set(Tuple(Symbol,Symbol))
-          state.each do |nextfloor|
-            if nextfloor == floor
-              #puts "|| => #{floor - option}"
-              newstate << (floor-option)
-            elsif lift_can_visit?(option,nextfloor)
-              #puts "|| ?> #{nextfloor + option}" 
-              newstate << (nextfloor + option)
-            else
-              #puts "||  > #{nextfloor}" 
-              newstate << nextfloor
+
+      options = generate_lifts(floor, lift)
+      options.each do |option|
+        taken, left = option
+        #puts "->#{option}"
+        if curfloor >0 #Go DOWN
+          newfloors = floors.dup
+          newfloors[curfloor] -= taken
+          newfloors[curfloor] += left
+
+          newfloor = curfloor-1
+          targetfloor = floors[newfloor]
+          if valid?(Set.new([] of Item), newfloors[curfloor]) &&
+             lift_can_visit?(taken, targetfloor)
+
+            newstate = {newfloor, taken, newfloors}
+            unless visited[newstate]? || generated[newstate]?
+              newstates << newstate
+              generated[newstate] = true
             end
           end
-          #puts "=" * 50
-          unless visited[newstate]? || generated[newstate]?
-            generated[newstate] = true
-            newstates << newstate 
+        end
+
+        if curfloor <3 #Go UP
+          newfloors = floors.dup
+          newfloors[curfloor] -= taken
+          newfloors[curfloor] += left
+
+          newfloor = curfloor+1
+          targetfloor = floors[newfloor]
+          if valid?(Set.new([] of Item), newfloors[curfloor]) &&
+             lift_can_visit?(taken, targetfloor)
+
+            newstate = {newfloor, taken, newfloors}
+            unless visited[newstate]? || generated[newstate]?
+              newstates << newstate
+              generated[newstate] = true
+            end
           end
         end
       end
@@ -118,28 +160,52 @@ class Puzzle
   end
 
   def target?(state)
-    state[0].empty? &&
-    state[1].empty? &&
-    state[2].empty?
+    floor, lift, floors = state
+    floors[0].empty? &&
+    floors[1].empty? &&
+    floors[2].empty?
+  end
+
+  def dump_states(states)
+    puts "="*50
+    states.each do |state|
+      dump_state(state)
+      puts "="*50
+    end
+  end
+
+  def dump_state(state)
+    floor, elevator, floors = state
+    floors.each_with_index do |f, idx|
+      print "#{idx} || #{f}"
+      if idx == floor
+        puts " || #{elevator}"
+      else
+        puts
+      end
+    end
   end
 
   def result
-    state = INPUT.dup
+    elevator = Set.new([] of Item) #empty
+
+    state = {2, elevator, INPUT.dup } #Start on the 3rd Floor, empty elevator
     step = 0
     
-    visited = {} of Array(Set(Tuple(Symbol,Symbol))) => Bool
+    visited = {} of State => Bool
     nextstates = [state]
 
-    10.times do 
-    nextstates, visited = next_states(nextstates,visited)
-    step += 1
-    puts "Step #{step} #{nextstates.size} new states, #{visited.keys.size} visited"
-    if nextstates.any? {|s| target?(s) }
-      puts "FOUND!"
-      raise "STOP"
-    else
-      puts "LOOP"
-    end
+    20.times do 
+      nextstates, visited = next_states(nextstates,visited)
+      step += 1
+      #dump_states(nextstates)
+      #puts "|" * 50
+      puts "Step #{step} #{nextstates.size} new states, #{visited.keys.size} visited"
+      #puts "|" * 50
+      if nextstates.any? {|s| target?(s) }
+        puts "FOUND!"
+        raise "STOP"
+      end
     end
   end
 
