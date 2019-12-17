f304482fbf8ad93a7b5acacc5cf2033004080a68
Jonathan Lozinski <jonathan.lozinski@arm.com>
2019-12-17 15:05:06 +0000

    WIP chunks manually pulled, output not right as yet

--- 2019/17/src/puzzle.cr
+++ 2019/17/src/puzzle.cr
@@ -11,9 +11,30 @@ class Puzzle
 
   def result
     robot = VacuumRobot.new
-    machine.execute([] of Int64, robot)
-    puts robot.render
-    p robot.build_route
-  end
+   # puts robot.render
+   # route = robot.build_route
+   # puts route
+    main = "A,B,B,A,C,C,B,C"
+    fa = "R,8,L,12,R,8"
+    fb = "R,12,L,8,R,10"
+    fc = "R,8,L,8,L,8,R,8,R,10"
 
+    input = main.chars.map {|c| c.ord }
+    input << 10 
+    input += fa.chars.map {|c| c.ord }
+    input << 10
+    input += fb.chars.map {|c| c.ord }
+    input << 10
+    input += fc.chars.map {|c| c.ord }
+    input << 10
+    input << 'n'.ord  #No Feed
+    input << 10
+
+    i64 = input.map {|i| i.to_i64}
+
+    machine.memory[0] = 2
+    output = [] of Int64
+    machine.execute(i64, output)
+    puts output
+  end
 end
