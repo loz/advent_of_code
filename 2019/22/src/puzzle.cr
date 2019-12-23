7e94ba92eba4e88e65e9563ae84458838f9fb02d
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2019-12-23 22:55:29 +0000

    Solved A Function for Confguent n (mod X)

--- 2019/22/src/puzzle.cr
+++ 2019/22/src/puzzle.cr
@@ -1,10 +1,62 @@
 alias Rule = Tuple(Symbol, Int32)
 
+class Solution
+  property operands = {} of Int64 => Int64
+
+  def initialize(op : Operand)
+    @operands[op.o_of] = op.o_n
+  end
+
+  def initialize(@operands); end
+
+  def for(o_of)
+    @operands[o_of]
+  end
+
+  def *(i : Int64)
+    ops = @operands.dup
+    ops.keys.each do |o_of|
+      ops[o_of] *= i
+    end
+    Solution.new(ops)
+  end
+
+  def -(s : Solution)
+    ops = @operands.dup
+    s.operands.keys.each do |o_of|
+      o_n = s.operands[o_of]
+      if ops[o_of]?
+        ops[o_of] -= o_n
+      else
+        ops[o_of] = 0.to_i64 - o_n
+      end
+    end
+    Solution.new(ops)
+  end
+
+  def to_s
+    @operands.map {|o_of, o_n| "(#{o_n})#{o_of}" }.join ","
+  end
+end
+
+class Operand
+  property o_n : Int64
+  property o_of : Int64
+
+  def initialize(@o_n, @o_of)
+  end
+
+  def *(i : Int64)
+    Operand.new(@o_n * i, @o_of)
+  end
+end
+
 class Puzzle
   property rules = [] of Rule
   property deck = [] of Int64
   property desk = [] of Int64
   property cards : Int64
+  property congruent = {} of Int64 => Int64
 
   def initialize(@cards = 0.to_i64)
   end
@@ -61,41 +113,49 @@ class Puzzle
   end
 
   def result_test
-    self.deck = (0...10).to_a
-    puts " - 9 - "
-    p self.deck
-    deal_with_increment(9)
-    p self.deck
-    rev = (0...10).to_a.map do |n|
-      reverse_digit_deal_with_increment(9,n)
+    @cards = 10
+    print "            "
+    10.times do |n|
+      print " #{n}"
     end
-    puts "What Mapped to X?"
-    puts rev
-    puts "*"*30
+    puts
+    print "CUT -4 FROM:"
+    10.times do |n|
+      from = reverse_digit_cut(-4,n)
+      print " #{from}"
+    end
+    puts
+    print "CUT  3 FROM:"
+    10.times do |n|
+      from = reverse_digit_cut(3,n)
+      print " #{from}"
+    end
+    puts
+    print "DNS    FROM:"
+    10.times do |n|
+      from = reverse_digit_deal_new_stack(n)
+      print " #{from}"
+    end
+    puts
+    print "DWI  3 FROM:"
+    10.times do |n|
+      from = reverse_digit_deal_with_increment(3,n)
+      print " #{from}"
+    end
+    puts
+    print "DWI  7 FROM:"
+    10.times do |n|
+      from = reverse_digit_deal_with_increment(7,n)
+      print " #{from}"
+    end
+    puts
+    print "DWI  9 FROM:"
+    10.times do |n|
+      from = reverse_digit_deal_with_increment(9,n)
+      print " #{from}"
+    end
+    puts
 
-    puts " - 3 - "
-    self.deck = (0...10).to_a
-    p self.deck
-    deal_with_increment(3)
-    p self.deck
-    rev = (0...10).to_a.map do |n|
-      reverse_digit_deal_with_increment(3,n)
-    end
-    puts "What Mapped to X?"
-    puts rev
-    puts "*"*30
-
-    puts " - 7 - "
-    self.deck = (0...10).to_a
-    p self.deck
-    deal_with_increment(7)
-    p self.deck
-    rev = (0...10).to_a.map do |n|
-      reverse_digit_deal_with_increment(7,n)
-    end
-    puts "What Mapped to X?"
-    puts rev
-    puts "*"*30
   end
 
   def result_search_unshuffled
@@ -136,8 +196,63 @@ class Puzzle
     puts calculate(2020)
   end
 
+  def expand_refs(n, refs)
+    if refs[n]?
+      val, mul, div = refs[n]
+      #puts "Expanding #{n}.. -> #{val} - #{mul}*#{div}.."
+      expand_refs(val,refs) - (expand_refs(div, refs) * mul )
+    else
+      Solution.new(Operand.new(1.to_i64,n.to_i64))
+    end
+  end
+
+  def calculate_w_values
+    values = @rules.select do |rule|
+      fn, n = rule
+      fn == :dwi
+    end.map {|rule| fn, n = rule; n }
+    values.uniq!
+    values.each do |n|
+      print  "Calculating Congruent for #{n} (mod #{cards}) -> "
+      ##ASSUME cards prime, and GCD(mod,cards) == 1 otherwise overlaps
+      refs = {} of Int64 => Tuple(Int64,Int64,Int64)
+      val = cards
+      div = n.to_i64
+      mul = val // div
+      rem = val % div
+      refs[rem] = {val,mul,div}
+      while rem > 1
+        #puts "#{val} = #{mul}*#{div} + #{rem}"
+        val = div
+        div = rem
+        rem = val % div
+        mul = val // div
+        refs[rem] = {val,mul,div}
+      end
+      #puts "#{val} = #{mul}*#{div} + #{rem}"
+      v = expand_refs(1, refs).for(n)
+      print v
+      if v < 0
+        v = cards - v
+        puts " -> #{v}"
+      else
+        puts
+      end
+      @congruent[n.to_i64] = v
+    end
+  end
+
   def result
-    result_search_unshuffled
+    count = if ARGV.empty?
+      #119315717514047
+      10007.to_i64
+    else
+      ARGV[0].to_i64
+    end
+    @cards = count
+    calculate_w_values
+    #result_test
+    #result_search_unshuffled
   end
 
   def find_repetition(unshuffled)
@@ -183,73 +298,39 @@ class Puzzle
     newcard
   end
 
+  def chinese_remainder(mods, rems)
+    max = 1
+    mods.each {|m| max = max * m }
+    series = rems.zip(mods).map {|r,m| r.step(to: max, by: m).to_a }
+    return nil if series.empty?
+    inall = series.first
+    series.each {|s| inall &= s }
+    return nil if inall.empty?
+    return inall.first
+  end
+
   def reverse_digit_deal_with_increment(n, card)
-  "
-- 9 -
-[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-[0, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-What Mapped to X?
-[0, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-******************************
-9 => 1 because 9 * 9 = 81  % 10 -> 1
-1> 10 * (9-1) = 80 | + 1 = 81 // 9 -> 9
+    #  n * C === card % cards
+    #  Linear Congruence
+    #  n and cards are co-prime otherwise overlaps..
+    mods = [] of Int64
+    rems = [] of Int64
+    val = cards
+    div = n.to_i64
+    rem = val % div
+    while rem > 1
+      mods << div
+      rems << rem
+      val = div
+      div = rem
+      rem = val % div
+    end
+    puts "***"
+    print ":>#{cards} % #{n} -> "
+    crem = chinese_remainder(mods, rems)
+    puts crem
 
-6 => 4 because 6 * 9 = 54 % 10 -> 4
-4> 10 * (9-4) = 50 | + 4 = 54 // 9 -> 6
-
-1 => 9 because 9 * 1 = 9 % 10 -> 9
-9> 10 * (9-9) = 0 | + 9 = 9 // 9 -> 1
-
-- 3 - 
-[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-[0, 7, 4, 1, 8, 5, 2, 9, 6, 3]
-What Mapped to X?
-[0, 3, 6, 9, 2, 5, 8, 1, 4, 7]
-******************************
-7 => 1 because 7 * 3 = 21, 21 % 10 -> 1
-1> 10 * (3-1) = 20  | + 1 = 21  // 3  -> 7
-
-4 => 2 because 4 * 3 = 12   % 10 -> 2
-2> 10 * (3-2) = 10  | + 2 = 12  // 3  -> 4
-
-8 => 4 because 8 * 3 = 24  % 10 -> 4
-4> 10 * (3-1) = 20  | + 4 = 24  // 3  -> 8
-
-- 7 -
-[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-[0, 3, 6, 9, 2, 5, 8, 1, 4, 7]
-
-7 => 9 because 7 * 7 = 49 % 10 -> 9
-9>  10 * (7-2) = 50 | + 9 = 59   !!!
-          7-3  = 40
-9> 70 - (9*7)|63 = 7
-
-6 => 2 because 7 * 6 = 42 % 10 -> 2
-2>  10 * (7-2) = 50 | + 2 = 52   !!!
-          7-3  = 40
-2> 70 - (2*7)|14 = 56 -> 6
-
-1 => 7 because 7 * 1 = 7 % 7 -> 7
-7>  10 * (7-0) = 70 | + 7 = 77 // 7 -> 11 -> 1
-
-"
-    # ORIGINAL FORMULA: cursor = (pos * n) % cards 
-    # REVERSE:  cursor = (pos * n) % cards
-    #           cursor = 0 - ((pos * n) % cards)
-    #cursor = CARDS - ((card * n) % cards)
-
-    #modn = card % n
-    #unmod = (cards * (n - modn)) + card
-    inv = inverse_mod(card, n, cards)
-    oldpos = inv//n
-    #unmod = (n * 10) - (n * card )
-    #oldpos = (unmod // n) % cards
-    #oldpos = unmod % cards
-
-    
-    # ORIGINAL FORMULA: cursor = (pos * n) % cards 
-    #p "Invert Deal: #{card}th of #{cards} (INC: @#{n}-> #{newcard}th"
-    oldpos
+    card
   end
 
   def inverse_mod(target, n, max)
