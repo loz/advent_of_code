27af2b489b36e7052dfe1c0b92ca0b0fc3ccb2c6
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2019-12-24 17:29:03 +0000

    Solved 2019-24

--- 2019/24/spec/puzzle_spec.cr
+++ 2019/24/spec/puzzle_spec.cr
@@ -1,11 +1,11 @@
 require "spec"
 require "./spec_helper"
 
-describe Puzzle do
+describe Layer do
 
   it "dies if not 1 bug adjacent" do
-    puzzle = Puzzle.new
-    puzzle.process <<-EOF
+    layer = Layer.new
+    layer.process <<-EOF
     #....
     .....
     .....
@@ -13,14 +13,14 @@ describe Puzzle do
     .....
     EOF
 
-    puzzle.at(0,0).should eq '#'
-    puzzle.generation
-    puzzle.at(0,0).should eq '.'
+    layer.at(0,0).should eq '#'
+    layer.generation
+    layer.at(0,0).should eq '.'
   end
 
   it "lives if 1 bug adjacent" do
-    puzzle = Puzzle.new
-    puzzle.process <<-EOF
+    layer = Layer.new
+    layer.process <<-EOF
     #....
     #....
     .....
@@ -28,14 +28,14 @@ describe Puzzle do
     .....
     EOF
 
-    puzzle.at(0,0).should eq '#'
-    puzzle.generation
-    puzzle.at(0,0).should eq '#'
+    layer.at(0,0).should eq '#'
+    layer.generation
+    layer.at(0,0).should eq '#'
   end
 
   it "infests empty if 1 or 2 bugs adjacent" do
-    puzzle = Puzzle.new
-    puzzle.process <<-EOF
+    layer = Layer.new
+    layer.process <<-EOF
     ##...
     #....
     .....
@@ -43,27 +43,27 @@ describe Puzzle do
     .....
     EOF
 
-    puzzle.generation
-    puzzle.at(0,0).should eq '.'
-    puzzle.at(1,1).should eq '#'
-    puzzle.at(3,3).should eq '.'
-    puzzle.at(2,3).should eq '#'
-    puzzle.at(4,3).should eq '#'
-    puzzle.at(3,2).should eq '#'
-    puzzle.at(3,4).should eq '#'
+    layer.generation
+    layer.at(0,0).should eq '.'
+    layer.at(1,1).should eq '#'
+    layer.at(3,3).should eq '.'
+    layer.at(2,3).should eq '#'
+    layer.at(4,3).should eq '#'
+    layer.at(3,2).should eq '#'
+    layer.at(3,4).should eq '#'
   end
 
   it "matches example" do
-    puzzle = Puzzle.new
-    puzzle.process <<-EOF
+    layer = Layer.new
+    layer.process <<-EOF
     ....#
     #..#.
     #..##
     ..#..
     #....
     EOF
-    puzzle.generation
-    puzzle.dump.should eq <<-EOF
+    layer.generation
+    layer.dump.should eq <<-EOF
     #..#.
     ####.
     ###.#
@@ -73,8 +73,8 @@ describe Puzzle do
   end
 
   it "calculates biodiversity rating" do
-    puzzle = Puzzle.new
-    puzzle.process <<-EOF
+    layer = Layer.new
+    layer.process <<-EOF
     ....#
     #..#.
     .....
@@ -82,6 +82,139 @@ describe Puzzle do
     .#...
     EOF
 
-    puzzle.diversity.should eq 16 + 32 + 256 + 32768 + 2097152
+    layer.diversity.should eq 16 + 32 + 256 + 32768 + 2097152
+  end
+
+  describe Puzzle do
+    it "gives no neighbors for centergrid" do
+      puzzle = Puzzle.new
+      puzzle.process <<-EOF
+      ....#
+      #..#.
+      #..##
+      ..#..
+      #....
+      EOF
+      puzzle.init_decks
+    
+      puzzle.neighbors(2,2,0).should eq( [] of Char)
+    end
+
+    it "calculates neighbors with inner left" do
+      puzzle = Puzzle.new
+      puzzle.process <<-EOF
+      ....#
+      #..#.
+      #..##
+      ..#..
+      #....
+      EOF
+      puzzle.init_decks
+      puzzle.neighbors(3,2,-1).count {|n| n == '#'}.should eq 2
+    end
+
+    it "calculates neighbors with inner right" do
+      puzzle = Puzzle.new
+      puzzle.process <<-EOF
+      ....#
+      #..#.
+      #..##
+      ..#..
+      #....
+      EOF
+      puzzle.init_decks
+      puzzle.neighbors(1,2,-1).count {|n| n == '#'}.should eq 3
+    end
+
+
+    it "calculates neighbors with inner bottom" do
+      puzzle = Puzzle.new
+      puzzle.process <<-EOF
+      ....#
+      #..#.
+      #..##
+      ..#..
+      #####
+      EOF
+      puzzle.init_decks
+      puzzle.neighbors(2,3,-1).count {|n| n == '#'}.should eq 5
+    end
+
+    it "calculates neighbors with inner top" do
+      puzzle = Puzzle.new
+      puzzle.process <<-EOF
+      ....#
+      #..#.
+      #..##
+      ..#..
+      #####
+      EOF
+      puzzle.init_decks
+      puzzle.neighbors(2,1,-1).count {|n| n == '#'}.should eq 1
+    end
+
+    it "calculates neighbors of outer top cells" do
+      puzzle = Puzzle.new
+      puzzle.process <<-EOF
+      .....
+      ..#..
+      .#.#.
+      .....
+      .....
+      EOF
+      puzzle.init_decks
+      (1..3).each do |x|
+        puzzle.neighbors(x,0,1).count {|n| n == '#'}.should eq 1
+      end
+      puzzle.neighbors(0,0,1).count {|n| n == '#'}.should eq 2
+      puzzle.neighbors(4,0,1).count {|n| n == '#'}.should eq 2
+    end
+
+    it "calculates neighbors of outer bottom cells" do
+      puzzle = Puzzle.new
+      puzzle.process <<-EOF
+      .....
+      .....
+      .#.#.
+      ..#..
+      .....
+      EOF
+      puzzle.init_decks
+      (1..3).each do |x|
+        puzzle.neighbors(x,4,1).count {|n| n == '#'}.should eq 1
+      end
+      puzzle.neighbors(0,4,1).count {|n| n == '#'}.should eq 2
+      puzzle.neighbors(4,4,1).count {|n| n == '#'}.should eq 2
+    end
+
+    it "calculates neighbors of outer left cells" do
+      puzzle = Puzzle.new
+      puzzle.process <<-EOF
+      .....
+      .....
+      .#...
+      .....
+      .....
+      EOF
+      puzzle.init_decks
+      (1..3).each do |y|
+        puzzle.neighbors(0,y,1).count {|n| n == '#'}.should eq 1
+      end
+    end
+
+    it "calculates neighbors of outer right cells" do
+      puzzle = Puzzle.new
+      puzzle.process <<-EOF
+      .....
+      .....
+      ...#.
+      .....
+      .....
+      EOF
+      puzzle.init_decks
+      (1..3).each do |y|
+        puzzle.neighbors(4,y,1).count {|n| n == '#'}.should eq 1
+      end
+    end
   end
 end
--- 2019/24/src/puzzle.cr
+++ 2019/24/src/puzzle.cr
@@ -1,11 +1,10 @@
 alias Row = Array(Char)
-alias Grid = Array(Row)
 
-class Puzzle
+class Layer
   @cells = [] of Row
   @gcells = [] of Row
-  @width = 0
-  @height = 0
+  @width = 5
+  @height = 5
 
   ADJACENT = [
     { 0, -1},
@@ -13,6 +12,24 @@ class Puzzle
     { 1,  0},
     { 0,  1},
   ]
+  def row(n)
+    @cells[n]
+  end
+
+  def each_row(&block)
+    @cells.each do |row|
+      yield row
+    end
+  end
+
+  def empty!
+    @cells = [] of Row
+    @gcells = [] of Row
+    @height.times do |y|
+      @cells << ['.'] * @width
+      @gcells << ['.'] * @width
+    end
+  end
 
   def process(str)
     str.lines.each do |line|
@@ -23,7 +40,7 @@ class Puzzle
     @height = @cells.size
   end
 
-  def neighbors(x,y)
+  def neighbors(x,y, layer)
     cells = [] of Char
     ADJACENT.each do |delta|
       dx, dy = delta
@@ -46,6 +63,16 @@ class Puzzle
     puts diversity
   end
 
+  def count
+    total = 0.to_u64
+    @cells.each do |row|
+      row.each do |ch|
+        total += 1 if ch == '#'
+      end
+    end
+    total
+  end
+
   def diversity
     total = 0.to_u64
     val = 1.to_u64
@@ -62,10 +89,21 @@ class Puzzle
     @cells.map {|row| row.join("")}.join("\n")
   end
 
-  def generation
+  def generation(neighbor_giver = self, layer = 0)
+    generation_without_tick(neighbor_giver, layer)
+    tick!
+  end
+
+  def tick!
+    tmp = @cells
+    @cells = @gcells
+    @gcells = tmp
+  end
+
+  def generation_without_tick(neighbor_giver, layer)
     @cells.each_with_index do |row, y|
       row.each_with_index do |ch, x|
-        ncells = neighbors(x,y).count {|n| n == '#'}
+        ncells = neighbor_giver.neighbors(x,y, layer).count {|n| n == '#'}
         if ch == '#' && ncells != 1
           @gcells[y][x] = '.' 
         elsif ch == '.' && (ncells == 1 || ncells == 2)
@@ -75,13 +113,117 @@ class Puzzle
         end
       end
     end
-    tmp = @cells
-    @cells = @gcells
-    @gcells = tmp
   end
 
   def at(x,y)
     @cells[y][x]
   end
+end
 
+class Puzzle
+  @layer = Layer.new
+
+  @depth = 105
+
+  property deck = {} of Int32 => Layer
+
+  def process(str)
+    @layer.process(str)
+  end
+
+  def neighbors(x,y,level) #nRecurisive Version
+    return [] of Row if x == 2 && y == 2
+    layer = @deck[level]
+    cells = layer.neighbors(x,y,0)
+    #OUTER
+    if y == 0 || y == 4 #Top/Bottom Row, include center top
+      return cells unless @deck[level-1]?
+      outer = @deck[level-1]
+      row = y == 0 ? 1 : 3
+      cells << outer.row(row)[2]
+      cells << outer.row(2)[1] if x == 0
+      cells << outer.row(2)[3] if x == 4
+    elsif x == 0 || x == 4
+      return cells unless @deck[level-1]?
+      outer = @deck[level-1]
+      row = outer.row(2)
+      col = x == 0 ? 1 : 3
+      cells << row[col]
+    #INNER
+    elsif y == 2 #Inner Left / Right
+      return cells unless @deck[level+1]?
+      inner = @deck[level+1]
+      col = x == 1 ? 0 : 4
+      inner.each_row do |row|
+        cells << row[col]
+      end
+    elsif x == 2 #Inner Bottom / Top
+      return cells unless @deck[level+1]?
+      inner = @deck[level+1]
+      #col = x == 1 ? 0 : 4
+      row = y == 1 ? 0 : 4
+      inner.row(row).each do |c|
+        cells << c
+      end
+    end
+    cells
+  end
+
+  def result_part1
+    seen = {@layer.dump => true}
+    @layer.generation
+    while !seen[@layer.dump]?
+      seen[@layer.dump] = true
+      @layer.generation
+    end
+    puts "*** REPEATED ***"
+    puts @layer.dump
+    puts @layer.diversity
+  end
+
+  def init_decks
+    deck[0] = @layer
+    ((0-@depth)...0).each do |d|
+      l = Layer.new
+      l.empty!
+      deck[d] = l
+    end
+    (1..@depth).each do |d|
+      l = Layer.new
+      l.empty!
+      deck[d] = l
+    end
+  end
+
+  def dump
+    ((0-@depth)..@depth).each do |level|
+      layer = @deck[level]
+      puts "-- #{level} --"
+      puts layer.dump
+      puts " (#{layer.count}) cells"
+    end
+  end
+
+  def generation
+    @deck.each do |level, layer|
+      layer.generation_without_tick(self, level)
+    end
+    @deck.each do |_, layer|
+      layer.tick!
+    end
+  end
+
+  def result_part2
+    init_decks
+    200.times do
+      generation
+    end
+    dump
+    total = @deck.sum {|_, layer| layer.count }
+    puts "After 200 mins, #{total} bugs"
+  end
+
+  def result
+    result_part2
+  end
 end
