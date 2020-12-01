86f48a052dd8d406d7b6ef0ceb86f743de28da74
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2020-12-01 09:52:07 +0000

    Add python 2020 template

---
+++ 2020/template_py/puzzle.py
@@ -0,0 +1,13 @@
+class Puzzle:
+
+  def process(this, text):
+    pass
+
+  def result(this):
+    pass
+
+if __name__ == '__main__':
+  puz = Puzzle()
+  inp = open('input', 'r').read()
+  puz.process(inp)
+  puz.result()
