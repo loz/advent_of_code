83b4389ac2efb7a2ac638756aab9dea57e72e409
Jonathan Lozinski <jonathan.lozinski@dayshape.com>
2021-12-03 05:13:38 -0500

    Day 03, solved

--- 2021/03/puzzle.py
+++ 2021/03/puzzle.py
@@ -2,59 +2,102 @@ class Puzzle:
   def __init__(self):
     self.gamma = 0
     self.epsilon = 0
+    self.o2rating = 0
+    self.co2rating = 0
     self.bit_counts = {}
+    self.bitsets = []
 
   def process(self, text):
     lines = text.split('\n')
     for line in lines:
       self.process_line(line)
-    self.compute_gamma()
-    self.compute_epsilon()
+    gamma = self.compute_gamma(self.bitsets)
+    self.gamma = int(''.join(gamma), 2)
+    epsilon = self.compute_epsilon(self.bitsets)
+    self.epsilon = int(''.join(epsilon), 2)
+    self.compute_o2rating()
+    self.compute_co2rating()
 
-  def compute_gamma(self):
+
+  def narrow(self, pos, bit, sets = None):
+    if sets == None:
+      sets = self.bitsets
+    matching = []
+    for bits in sets:
+      if bits[pos] == bit:
+        matching.append(bits)
+    return matching
+
+  def compute_o2rating(self):
+    bitsets = self.bitsets
+    bit = 0
+    while len(bitsets) > 1:
+      gamma = self.compute_gamma(bitsets)
+      bitsets = self.narrow(bit, gamma[bit], bitsets)
+      bit+=1
+    self.o2rating = int(''.join(bitsets[0]), 2)
+
+  def compute_co2rating(self):
+    bitsets = self.bitsets
+    bit = 0
+    while len(bitsets) > 1:
+      epsilon = self.compute_epsilon(bitsets)
+      bitsets = self.narrow(bit, epsilon[bit], bitsets)
+      bit+=1
+    self.co2rating = int(''.join(bitsets[0]), 2)
+
+  def compute_gamma(self, bitsets):
     gamma = []
-    numbits = len(self.bit_counts) / 2
-    #print numbits
-    #print len(self.bit_counts)
-    #print self.bit_counts
+    bit_counts = self.count_bits(bitsets)
+    numbits = len(bitsets[0])
     for bit in range(numbits):
-      zero_count = self.bit_counts[(bit,'0')]
-      one_count = self.bit_counts[(bit,'1')]
+      zero_count = bit_counts.get((bit,'0'),0)
+      one_count =  bit_counts.get((bit,'1'),0)
       if zero_count > one_count:
         gamma.append('0')
       else:
         gamma.append('1')
-      
-    self.gamma = int(''.join(gamma), 2)
+    return gamma
 
-  def compute_epsilon(self):
+  def compute_epsilon(self, bitsets):
     epsilon = []
-    numbits = len(self.bit_counts) / 2
-    #print numbits
-    #print len(self.bit_counts)
-    #print self.bit_counts
+    bit_counts = self.count_bits(bitsets)
+    numbits = len(bitsets[0])
     for bit in range(numbits):
-      zero_count = self.bit_counts[(bit,'0')]
-      one_count = self.bit_counts[(bit,'1')]
-      if zero_count < one_count:
+      zero_count = bit_counts.get((bit,'0'),0)
+      one_count =  bit_counts.get((bit,'1'),0)
+      if zero_count <= one_count:
         epsilon.append('0')
       else:
         epsilon.append('1')
-      
-    self.epsilon = int(''.join(epsilon), 2)
+    return epsilon
+
+  def count_bits(self, bitset):
+    bit_counts = {}
+    for bits in bitset:
+      pos = 0
+      for bit in bits:
+        count = bit_counts.get((pos,bit), 0)
+        count +=1
+        bit_counts[(pos,bit)] = count
+        pos += 1
+    return bit_counts
 
   def process_line(self, line):
-    pos = 0
+    if len(line) == 0:
+      return
+    bits = []
     for bit in line:
-      count = self.bit_counts.get((pos,bit), 0)
-      count +=1
-      self.bit_counts[(pos,bit)] = count
-      pos += 1
+      bits.append(bit)
+    self.bitsets.append(bits)
 
   def result(self):
     print 'Gamma', self.gamma
     print 'Epsilon', self.epsilon
     print 'Result', self.gamma * self.epsilon
+    print 'O2 Rating', self.o2rating
+    print 'CO2 Rating', self.co2rating
+    print 'Result', self.o2rating * self.co2rating
 
 if __name__ == '__main__':
   puz = Puzzle()
--- 2021/03/puzzle.test.py
+++ 2021/03/puzzle.test.py
@@ -21,5 +21,33 @@ class TestPuzzle(unittest.TestCase):
     #most common = 010110
     self.assertEqual(puzzle.epsilon, 0b101001)
 
+
+  def test_puzzle_narrows_bitsets(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""010110
+111111
+000000
+""")
+    bitsets = puzzle.narrow(0, '1')
+    self.assertEqual(bitsets, [['1', '1', '1', '1', '1', '1']])
+
+  def test_puzzle_computes_o2rating(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""010110
+111100
+100000
+""")
+    self.assertEqual(puzzle.o2rating, 0b111100)
+
+  def test_puzzle_computes_co2rating(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""010110
+111100
+000011
+100000
+""")
+    self.assertEqual(puzzle.co2rating, 0b000011)
+    
+
 if __name__ == '__main__':
     unittest.main()
