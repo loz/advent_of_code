e1a2eccfa5fc2460e82207e06856eaa9d6a52797
Jonathan Lozinski <jonathan.lozinski@dayshape.com>
2021-12-08 19:21:22 +0000

    Day 08, wip

--- 2021/08/puzzle.py
+++ 2021/08/puzzle.py
@@ -1,17 +1,95 @@
-class Puzzle:
+class Puzzle: 
 
   def process(self, text):
     self.outputs = []
+    self.inputs = []
+    self.bitmasks = {
+     0: 0b1110111,
+     1: 0b0010010,
+     2: 0b1011101,
+     3: 0b1011011,
+     4: 0b0111010,
+     5: 0b1101011,
+     6: 0b1101111,
+     7: 0b1010010,
+     8: 0b1111111,
+     9: 0b1111011
+    }
+
     lines = text.split('\n')
     for line in lines:
       self.process_line(line)
+        
+  def toset(self, pattern):
+    return set([ch for ch in pattern])
+
+  def toskey(self, key):
+    skey = list(key)
+    skey.sort()
+    return ''.join(skey)
+    
+
+  def genmasks(self, patterns):
+    masks = {}
+    known = []
+    unknown = map(lambda p: self.toset(p), patterns.split(' '))
+    for pattern in patterns.split(' '):
+      opts = self.map_options(pattern)
+      key =  self.toset(pattern)
+      skey = self.toskey(key)
+      if len(opts) == 1:
+        known.append(key)
+        unknown.remove(key)
+        masks[skey] = opts[0]
+      else:
+        masks[skey] = opts
+    print known, unknown
+    if len(unknown) > 0:
+      print 'More to Sort', masks
+      for item in unknown:
+        skey = self.toskey(item)
+        for kitem in known:
+          if kitem.issubset(item):
+            print 'Known In Unknown', kitem
+            kmask = masks[self.toskey(kitem)]
+            umask = masks[skey]
+            print self.bin_strs([kmask]), 'v', self.bin_strs(umask)
+            matches = []
+            for m in umask:
+              if (kmask & m) == kmask:
+                matches.append(m)
+            if len(matches) == 1:
+              unknown.remove(item)
+              masks[skey] = matches[0]
+              break
+            else:
+              masks[skey] = matches
+    return masks
+
+  def bin_strs(self, arr):
+    return '[' + ','.join(map(lambda b: "{:08b}".format(b), arr)) + ']'
 
   def process_line(self, line):
     if len(line) > 0:
       patterns, digits = line.split(' | ')
-      print digits
+      self.inputs.append((patterns, digits))
       numbers = self.map_digits(digits)
       self.outputs.append(numbers)
+
+  def map_options(self, signal):
+    l = len(signal)
+    if l == 2:
+      return [self.bitmasks[1]]
+    elif l == 3:
+      return [self.bitmasks[7]]
+    elif l == 4:
+      return [self.bitmasks[4]]
+    elif l == 5:
+      return [self.bitmasks[2], self.bitmasks[3], self.bitmasks[5]]
+    elif l == 6:
+      return [self.bitmasks[0], self.bitmasks[6], self.bitmasks[9]]
+    elif l == 7:
+      return [self.bitmasks[8]]
 
   def map_digits(self, digits):
     digits = digits.split(' ')
@@ -28,12 +106,25 @@ class Puzzle:
         mapped.append(8)
     return mapped
 
-  def result(self):
+  def result1(self):
     total = 0
     for output in self.outputs:
       print output
       total += len(output)
     print 'Total 1, 4, 7, 8s:', total
+
+  def result(self):
+    for i in self.inputs:
+      print i
+    i = self.inputs[0]
+    pattern, digits = i
+    options = self.narrow_options(pattern)
+    for k in options:
+      print k, options[k]
+    options = self.narrow_options(pattern, options)
+    for k in options:
+      print k, options[k]
+
 
 if __name__ == '__main__':
   puz = Puzzle()
--- 2021/08/puzzle.test.py
+++ 2021/08/puzzle.test.py
@@ -3,12 +3,86 @@ import puzzle as puz
 
 class TestPuzzle(unittest.TestCase):
 
+  def test_puzzle_finds_bitmask_for_1(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""ab ab | ab
+""")
+    masks = puzzle.genmasks('ab')
+    mask = masks['ab']
+    self.assertEqual(mask, 0b0010010)
+
+  def test_puzzle_finds_bitmask_for_7(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""ab ab | ab
+""")
+    masks = puzzle.genmasks('abc')
+    mask = masks['abc']
+    self.assertEqual(mask, 0b1010010)
+
+  def test_puzzle_finds_bitmask_for_4(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""ab ab | ab
+""")
+    masks = puzzle.genmasks('abcd')
+    mask = masks['abcd']
+    self.assertEqual(mask, 0b0111010)
+
+  def test_puzzle_finds_bitmask_for_0(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""ab ab | ab
+""")
+    masks = puzzle.genmasks('be fedb gbe gafceb')
+    mask = masks['abcefg']
+    self.assertEqual(mask, 0b1110111)
+
+  def test_puzzle_finds_bitmask_for_3(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""ab ab | ab
+""")
+    masks = puzzle.genmasks('be fedb gbe gedba')
+    mask = masks['abdeg']
+    self.assertEqual(mask, 0b1011011)
+
+  def test_puzzle_finds_bitmask_for_2(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""ab ab | ab
+""")
+    masks = puzzle.genmasks('be fedb gbe gafceb gedba gedca')
+    mask = masks['acdeg']
+    self.assertEqual(mask, 0b1011101)
+
+
+  def test_puzzle_finds_bitmask_for_5(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""ab ab | ab
+""")
+    masks = puzzle.genmasks('be fedb gbe gfdba')
+    mask = masks['abdfg']
+    self.assertEqual(mask, 0b1101011)
+
+  def test_puzzle_finds_bitmask_for_6(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""ab ab | ab
+""")
+    masks = puzzle.genmasks('be fedb gbe gfdcba')
+    mask = masks['abcdfg']
+    self.assertEqual(mask, 0b1101111)
+
+  def test_puzzle_finds_bitmask_for_9(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""ab ab | ab
+""")
+    masks = puzzle.genmasks('be fedb gbe gfedba')
+    mask = masks['abdefg']
+    self.assertEqual(mask, 0b1111011)
+
   def test_puzzle_matches_1(self):
     puzzle = puz.Puzzle()
     puzzle.process("""ab ab | ab
 """)
     first = puzzle.outputs[0]
     self.assertEqual(first, [1])
+
 
   def test_puzzle_matches_4(self):
     puzzle = puz.Puzzle()
