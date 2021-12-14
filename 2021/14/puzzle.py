37f97099ca9a427a61458601445cb2418f8c3bb0
Jonathan Lozinski <jonathan.lozinski@dayshape.com>
2021-12-14 09:16:37 +0000

    Day 14 Solved

--- 2021/14/puzzle.py
+++ 2021/14/puzzle.py
@@ -1,3 +1,5 @@
+import math
+
 class Puzzle:
 
   def process(self, text):
@@ -5,6 +7,17 @@ class Puzzle:
     self.process_chain(lines[0])
     lines = lines[2:]
     self.process_reactions(lines)
+    self.build_pairs()
+
+  def build_pairs(self):
+    pairs = {}
+    for c in range(len(self.chain)-1):
+      pair = self.chain[c] + self.chain[c+1]
+      pc = pairs.get(pair, 0)
+      pc += 1
+      pairs[pair] = pc
+    self.pairs = pairs
+
 
   def process_chain(self, chain):
     chain = chain.rstrip()
@@ -18,6 +31,23 @@ class Puzzle:
         reactions[left] = right
     self.reactions = reactions
 
+  def expand_pairs(self):
+    print self.pairs
+    newpairs = {}
+    for pair in self.pairs:
+      pc = self.pairs[pair]
+      if pc > 0:
+        #print pc, self.reactions[pair]
+        r = self.reactions[pair]
+        left = pair[0]
+        right = pair[1]
+        nl = left + r
+        nr = r + right
+        newpairs[nl] = newpairs.get(nl, 0) + pc
+        newpairs[nr] = newpairs.get(nr, 0) + pc
+    self.pairs = newpairs
+        
+
   def expand(self):
     newchain = []
     for c in range(len(self.chain)-1):
@@ -27,12 +57,27 @@ class Puzzle:
     newchain.append(self.chain[c+1])
     self.chain = newchain
 
-  def min_max(self):
+  def pair_freqs(self):
+    freqs = {}
+    print self.pairs
+    for pair in self.pairs:
+      l = pair[0]
+      r = pair[1]
+      freqs[l] = freqs.get(l, 0) + self.pairs[pair]
+      freqs[r] = freqs.get(r, 0) + self.pairs[pair]
+    for freq in freqs:
+      freqs[freq] = math.ceil(freqs[freq]/2.0)
+    return freqs
+
+  def freqs(self):
     freqs = {}
     for c in self.chain:
       fc = freqs.get(c, 0)
       fc += 1
       freqs[c] = fc
+    return freqs
+    
+  def min_max(self, freqs):
     mmin = min(freqs.keys(), key=lambda f: freqs[f])
     mmax = max(freqs.keys(), key=lambda f: freqs[f])
     return ((mmin, freqs[mmin]), (mmax, freqs[mmax]))
@@ -41,10 +86,15 @@ class Puzzle:
     return ''.join(self.chain)
 
   def result(self):
-    for s in range(10):
-      self.expand()
-    print self.chain_string()
-    print self.min_max()
+    for s in range(40):
+      print 'Step', s
+      print self.pair_freqs()
+      #print self.min_max()
+      self.expand_pairs()
+    f = self.pair_freqs()
+    print self.min_max(f)
+    #print self.chain_string()
+    #print self.min_max()
 
 if __name__ == '__main__':
   puz = Puzzle()
--- 2021/14/puzzle.test.py
+++ 2021/14/puzzle.test.py
@@ -14,6 +14,47 @@ BC -> Y
     chain = puzzle.chain_string()
     self.assertEqual(chain, 'AXBYC')
 
+  def test_puzzle_tracks_pairs(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""ABC
+
+AB -> X
+BC -> Y
+""")
+    self.assertEqual(puzzle.pairs['AB'], 1)
+    self.assertEqual(puzzle.pairs['BC'], 1)
+
+  def test_puzzle_expands_pairs(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""ABC
+
+AB -> X
+BC -> Y
+""")
+    # -> AXBYC
+    puzzle.expand_pairs()
+    self.assertEqual(puzzle.pairs['AX'], 1)
+    self.assertEqual(puzzle.pairs['XB'], 1)
+    self.assertEqual(puzzle.pairs['BY'], 1)
+    self.assertEqual(puzzle.pairs['YC'], 1)
+
+  def test_puzzle_counts_freq_with_pairs(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""ABC
+
+AB -> X
+BC -> Y
+""")
+    # -> AXBYC
+    puzzle.expand_pairs()
+    freqs = puzzle.pair_freqs()
+    self.assertEqual(freqs['A'], 1)
+    self.assertEqual(freqs['B'], 1)
+    self.assertEqual(freqs['C'], 1)
+    self.assertEqual(freqs['Y'], 1)
+    self.assertEqual(freqs['X'], 1)
+
+
   def test_puzzle_calcs_min_max(self):
     puzzle = puz.Puzzle()
     puzzle.process("""ABABABCABABAAAC
