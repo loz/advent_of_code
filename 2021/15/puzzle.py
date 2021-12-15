afaef6b52d2647caf8c6215396e47c331f8b04e9
Jonathan Lozinski <jonathan.lozinski@dayshape.com>
2021-12-15 09:15:12 +0000

    Day 15, solved

--- 2021/15/puzzle.py
+++ 2021/15/puzzle.py
@@ -15,18 +15,57 @@ class Puzzle:
       row = [int(ch) for ch in line]
       self.rows.append(row)
 
+  def expand(self):
+    #expand in X
+    new_rows = []
+    for row in self.rows:
+      newrow = [r for r in  row]
+      for i in range(1,5):
+        for x in range(self.width):
+          n = newrow[(self.width*(i-1))+x]
+          n = (n % 9) + 1
+          newrow.append(n)
+      new_rows.append(newrow)
+    self.rows = new_rows
+    #expand in Y
+    new_rows = [r for r in self.rows]
+    for i in range(1,5):
+      for y in range(self.height):
+        row = [r for r in new_rows[(self.height*(i-1))+y]]
+        print row
+        newrow = []
+        for n in row:
+          m = (n % 9) + 1
+          newrow.append(m)
+        new_rows.append(newrow)
+    self.rows = new_rows
+    self.height = len(self.rows)
+    self.width = len(self.rows[0])
+
+
+  def to_str(self):
+    dump = ""
+    for row in self.rows:
+      for n in row:
+        dump += str(n)
+      dump += '\n'
+    return dump
+
   def naive_bests(self):
     bests = {(0,0): 0}
+    parents = {}
     for y in range(self.height):
       if y > 0:
         cost = bests[(0, y-1)] + self.rows[y][0]
+        parents[(0, y)]=(0,y-1)
       else:
         cost = 0
       row = self.rows[y]
       bests[(0, y)] = cost
       for x in range(1, self.width):
         bests[(x,y)] = bests[(x-1, y)] + self.rows[y][x]
-    return bests
+        parents[(x, y)]=(x-1,y)
+    return (bests, parents)
 
   def shortest_path(self):
     start = (0,0)
@@ -34,37 +73,38 @@ class Puzzle:
     print 'Plotting', start, end
     tovisit = [(start, [], 0)]
     bests = {start:sys.maxint, end: sys.maxint}
-    bests = self.naive_bests()
+    bests, parents = self.naive_bests()
     print 'Naive best', bests[end]
-    #return
-    #neighbours = self.neighbours(start)
-    #for n in neighbours:
-    #  tovisit.append((n, [start], bests[n]))
-    bestpaths = {start:[]}
-    while len(tovisit):
-      cur = tovisit.pop()
-      #print cur
-      loc, path, cost = cur
-      bloc = bests.get(loc, sys.maxint)
-      if cost <= bloc and cost < bests[end]:
-        bests[loc] = cost
-        bestpaths[loc] = path + [loc]
-        if loc == end:
-          print 'Found path', cost, 'remaining', len(tovisit)
-        neighbours = self.neighbours(loc)
+    #print bests
+    #print parents
+    changed = set([p for p in parents])
+    while len(changed) > 0:
+      print 'Processing', len(changed), 'changes'
+      newchanged = set()
+      for item in changed:
+        neighbours = self.neighbours(item)
+        #print item, neighbours
+        pcost = bests[item]
         for neighbour in neighbours:
-          nx, ny = neighbour
-          ncost = cost + self.rows[ny][nx]
-          nbest = bests.get(neighbour, sys.maxint)
-          #only do if this is shortest path to here
-          if ncost <= nbest: 
-            #print 'Should visit', neighbour, ncost
-            tovisit.append((neighbour, path + [loc], ncost))
-    return (bestpaths[end], bests[end])
+          x, y = neighbour
+          cost = self.rows[y][x]
+          if bests[neighbour] > pcost + cost:
+            #This is better now
+            bests[neighbour] = pcost + cost
+            parents[neighbour] = item
+            newchanged.add(neighbour)
+      changed = newchanged
+    cur = end
+    path = []
+    while cur != start:
+      path = [cur] + path
+      cur = parents[cur]
+    path = [start] + path
+    return (path, bests[end])
 
   def neighbours(self, loc):
     x, y = loc
-    deltas = [(1, 0), (0, 1)]
+    deltas = [(-1, 0), (0, -1), (1, 0), (0, 1)]
     neighbours = []
     for d in deltas:
       dx, dy = d
@@ -75,10 +115,12 @@ class Puzzle:
     return neighbours
 
   def result(self):
+    self.expand()
+    print self.to_str()
     print self.shortest_path()
 
 if __name__ == '__main__':
   puz = Puzzle()
-  inp = open('input.1', 'r').read()
+  inp = open('input', 'r').read()
   puz.process(inp)
   puz.result()
--- 2021/15/puzzle.test.py
+++ 2021/15/puzzle.test.py
@@ -12,5 +12,30 @@ class TestPuzzle(unittest.TestCase):
     path, distance = puzzle.shortest_path()
     self.assertEqual(path, [(0,0), (0,1), (0, 2), (1,2), (2,2)])
 
+  def test_puzzle_expands_map(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""122
+122
+111
+""")
+    puzzle.expand()
+    print puzzle.to_str()
+    self.assertEqual(puzzle.to_str(), """122233344455566
+122233344455566
+111222333444555
+233344455566677
+233344455566677
+222333444555666
+344455566677788
+344455566677788
+333444555666777
+455566677788899
+455566677788899
+444555666777888
+566677788899911
+566677788899911
+555666777888999
+""")
+
 if __name__ == '__main__':
     unittest.main()
