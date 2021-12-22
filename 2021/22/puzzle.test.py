1fc311e565ff17e7970eee75e14498ac8114d354
Jonathan Lozinski <jonathan.lozinski@dayshape.com>
2021-12-22 16:13:23 +0000

    Added some references for remove (off) cubes

--- 2021/22/puzzle.test.py
+++ 2021/22/puzzle.test.py
@@ -79,5 +79,34 @@ off x=2..3,y=0..1,z=2..2
     self.assertFalse((2,0,2) in puzzle.on)
     self.assertFalse((3,1,2) in puzzle.on)
 
+  def test_remove_separate(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""on x=0..1,y=0..1,z=0..1
+off x=2..3,y=2..3,z=2..3
+""")
+    self.assertEqual(puzzle.count_on(), 8)
+
+  def test_remove_corner_overlap(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""on x=0..2,y=0..2,z=0..2
+off x=2..3,y=2..3,z=2..3
+""")
+    self.assertEqual(puzzle.count_on(), 26)
+
+  def test_remove_side_overlap(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""on x=0..4,y=0..4,z=0..2
+off x=2..3,y=2..3,z=2..3
+""")
+    self.assertEqual(puzzle.count_on(), 71)
+
+  def test_remove_consume(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""on x=2..2,y=2..2,z=2..2
+off x=1..3,y=1..3,z=1..3
+on x=1..2,y=2..2,z=2..2
+""")
+    self.assertEqual(puzzle.count_on(), 2)
+
 if __name__ == '__main__':
     unittest.main()
