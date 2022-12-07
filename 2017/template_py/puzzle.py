54337a4af20b06491d09c73bb73d0bfc9c81581a
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2022-12-07 17:11:36 +0000

    Updated 2017 template

--- 2017/template_py/puzzle.py
+++ 2017/template_py/puzzle.py
@@ -1,13 +1,25 @@
+import sys
+
 class Puzzle:
 
   def process(self, text):
-    pass
+    for line in text.split('\n'):
+      self.process_line(line)
+
+  def process_line(self, line):
+    if line != '':
+      pass
 
   def result(self):
     pass
 
+
+
 if __name__ == '__main__':
   puz = Puzzle()
-  inp = open('input', 'r').read()
+  inputfile = 'input'
+  if len(sys.argv) == 2:
+    inputfile = sys.argv[1]
+  inp = open(inputfile, 'r').read()
   puz.process(inp)
   puz.result()
