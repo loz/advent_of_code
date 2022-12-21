45f97990abc413a4d99c2b7a5681fe4e6d7fef5b
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2022-12-21 06:20:45 +0000

    Some optimisation for fun

--- 2022/19/puzzle.py
+++ 2022/19/puzzle.py
@@ -41,14 +41,10 @@ class Puzzle:
     return (name, (ore, clay, obsidian))
 
   def can_afford(self, costs, resources):
-    ca, cb, cc   = costs
-    ra, rb, rc,_ = resources
-    return (ca <= ra and cb <= rb and cc <= rc)
+    return (costs[0] <= resources[0] and costs[1] <= resources[1] and costs[2] <= resources[2])
 
   def build(self, costs, resources):
-    ca, cb, cc   = costs
-    ra, rb, rc, rd  = resources
-    return (ra-ca, rb-cb, rc-cc, rd)
+    return (resources[0]-costs[0],resources[1]-costs[1],resources[2]-costs[2], resources[3])
 
   def gen_choices(self, blueprint, resources):
     costs = self.blueprints[blueprint]
@@ -63,47 +59,10 @@ class Puzzle:
         options.append(item)
     return options
     
-  def gen_choices_2(self, blueprint, resources):
-    costs = self.blueprints[blueprint]
-    options = []
-    geo = costs["geode"]
-    if self.can_afford(geo, resources):
-      #build it!
-      return ["geode"]
-    if resources[2] >= geo[2]:
-      return [None] #Wait for resources
-
-    obs = costs["obsidian"]
-    if self.can_afford(obs, resources):
-      #build it!
-      return ["obsidian"]
-    if resources[1] >= obs[1]:
-      return [None] #Wait for resources
-    for item in ["ore", "clay"]:
-      needs = costs[item]
-      if self.can_afford(needs, resources):
-        options.append(item)
-    options.append(None)
-    return options
-
   def collect(self, resources, robots):
-    amounts = {
-      "ore":resources[0],
-      "clay":resources[1],
-      "obsidian":resources[2],
-      "geode":resources[3],
-    }
-    for robot in robots.keys():
-      amounts[robot] += robots[robot]
-      #print robots[robot], robot, 'collect, you have now have', amounts[robot]
-    return (amounts['ore'], amounts['clay'], amounts['obsidian'], amounts['geode'])
+    return(resources[0] + robots["ore"], resources[1] + robots["clay"], resources[2] + robots["obsidian"], resources[3] + robots["geode"])
 
   def build(self, robot, resources, robots, idx):
-    amounts = {
-      "ore":resources[0],
-      "clay":resources[1],
-      "obsidian":resources[2],
-    }
     costs = self.blueprints[idx][robot]
     resources = (resources[0]-costs[0], resources[1]-costs[1], resources[2]-costs[2], resources[3])
 
@@ -159,9 +118,11 @@ class Puzzle:
     best = None
     robots = {"ore":1, "clay":0, "obsidian":0, "geode":0}
     resources = (0, 0, 0, 0)
-    tovisit = [(robots, resources, 0, [])]
+    #tovisit = [(robots, resources, 0, [])]
+    tovisit = [(robots, resources, 0)]
     while tovisit:
-      robots, resources, timer, path = tovisit.pop()
+      #robots, resources, timer, path = tovisit.pop()
+      robots, resources, timer = tovisit.pop()
       item = (robots["ore"], robots["clay"], robots["obsidian"], robots["geode"], resources, timer)
       if visited.get(item, False):
         continue
@@ -176,9 +137,9 @@ class Puzzle:
       if timer == timelimit:
         if resources[3] > maxgeod:
           maxgeod = resources[3]
-          maxrobot = path
-          best = robots
-        allpaths.append(path)
+          #maxrobot = path
+          #best = robots
+        #allpaths.append(path)
         continue
       #print '== Minute ', timer, '=='
       #if timer == viable[1] and robots["clay"] == 0:
@@ -192,12 +153,14 @@ class Puzzle:
         if option == None: #Wait option
           nrobots = robots.copy()
           nresources = self.collect(resources, robots)
-          tovisit.append((nrobots, nresources, timer+1, path + [(nrobots, nresources)]))
+          #tovisit.append((nrobots, nresources, timer+1, path + [(nrobots, nresources)]))
+          tovisit.append((nrobots, nresources, timer+1))
         else:
           if robots.get(option, 0) < limits[option]:
             nrobots, nresources = self.build(option, resources, robots, idx)
             nresources = self.collect(nresources, robots)
-            tovisit.append((nrobots, nresources, timer+1, path + [(robots, nresources)]))
+            #tovisit.append((nrobots, nresources, timer+1, path + [(robots, nresources)]))
+            tovisit.append((nrobots, nresources, timer+1))
 
     #for path in allpaths:
     #  print '='*80
@@ -224,14 +187,14 @@ class Puzzle:
     blueprint = 0
     total = 0
     for blueprint in range(len(self.blueprints)):
-    #for blueprint in [8]:
+    #for blueprint in [1]:
       print 'Running BP:', blueprint+1
       val = self.run_blueprint(blueprint)
       print val, 'quality', (blueprint+1)*val
       total += ((blueprint+1)*val)
     print 'Total', total
 
-  def result(self):
+  def result2(self):
     blueprint = 0
     total = 0
     #for blueprint in range(len(self.blueprints)):
@@ -241,9 +204,6 @@ class Puzzle:
       print val, 'quality', (blueprint+1)*val
       total += ((blueprint+1)*val)
     print 'Total', total
-      
-
-
 
 if __name__ == '__main__':
   puz = Puzzle()
