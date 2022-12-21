dfe8778af0d52e8574d06110eb4a096774179cd5
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2022-12-21 17:40:39 +0000

    Implement a-star for day 12

--- 2022/12/puzzle.py
+++ 2022/12/puzzle.py
@@ -64,6 +64,65 @@ class Puzzle:
     for o in options:
       if o not in visited:
         self.bfs(o, visited + [loc], distance +1)
+ 
+  def a_h(self, a, b):
+    #max of manhatten distance and height delta
+    ax, ay = a
+    bx, by = b
+    #ah = self.heights[ay][ax]
+    #bh = self.heights[by][bx]
+    #if ah == 'S':
+    #  ah = 'a'
+    #if bh == 'E':
+    #  bh = 'z'
+
+    #hdelta = ord(bh) - ord(ah)
+    dist = abs(bx-ax) + abs(by-ay)
+    #return max(hdelta, dist)
+    return dist
+
+  def astar(self, start):
+    #Infinity all cells
+    g_score = {}
+    f_score = {}
+    for y in range(len(self.heights)):
+      for x in range(len(self.heights[0])):
+        g_score[(x,y)] = sys.maxsize
+        f_score[(x,y)] = sys.maxsize
+    g_score[start] = 0
+    start_h = self.a_h(start,self.end)
+    f_score[start] = start_h
+    pq = []
+    heapq.heapify(pq)
+    heapq.heappush(pq, [(start_h, start_h), start])
+    paths = {}
+    found = False
+    while pq:
+      score, loc = heapq.heappop(pq)
+      if loc == self.end:
+        found = True
+        break
+      options = self.moves_from(loc[0], loc[1])
+      for option in options:
+        g_s = g_score[loc]+1
+        a_h = self.a_h(option, self.end)
+        f_s = g_s + a_h
+        #print option, (g_s, f_s), f_score[option]
+
+        if f_s < f_score[option]:
+          g_score[option] = g_s
+          f_score[option] = f_s
+          heapq.heappush(pq, [(f_s, a_h), option])
+          paths[option] = loc
+    if not found:
+      return None
+
+    fwdpath = {}
+    cell = self.end
+    while cell != start:
+      fwdpath[paths[cell]] = cell
+      cell = paths[cell]
+    return len(fwdpath.keys()), fwdpath.keys()
 
   def dijk(self, start):
     pq = []
@@ -88,7 +147,7 @@ class Puzzle:
     
   def result1(self):
     d, path = self.dijk(self.start)
-    print path
+    #d, path = self.astar(self.start)
     for y in range(len(self.heights)):
       for x in range(len(self.heights[0])):
         h = self.map(x, y)
@@ -97,7 +156,7 @@ class Puzzle:
         else:
           sys.stdout.write(u"\u001b[0m" + h)
       print u"\u001b[0m"
-
+    print 'Path length', d
 
   def result(self):
     starts = []
@@ -108,10 +167,13 @@ class Puzzle:
         if h == 'S' or h == 'a':
           starts.append((x, y))
     for start in starts:
-      dist = self.dijk(start)
-      if dist != None and dist < shortest:
-        shortest = dist
-      print start, dist
+      #dist = self.dijk(start)
+      dist = self.astar(start)
+      if dist != None and dist[0] < shortest:
+        shortest = dist[0]
+      #print start, dist
+      print '.',
+    print
     print 'Shortest start gives', shortest
 
 if __name__ == '__main__':
@@ -121,4 +183,4 @@ if __name__ == '__main__':
     inputfile = sys.argv[1]
   inp = open(inputfile, 'r').read()
   puz.process(inp)
-  puz.result1()
+  puz.result()
