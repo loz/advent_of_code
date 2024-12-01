4b0890946e8eb24edec4042f7f5e404dd16d6be1
Jonathan Lozinski <jonathan.lozinski@dayshape.com>
2024-12-01 07:02:07 +0000

    Fix borked template

--- 2024/template.py/puzzle.py
+++ 2024/template.py/puzzle.py
@@ -15,14 +15,12 @@ class Puzzle:
     pass
 
 
-
 if __name__ == '__main__':
   puz = Puzzle()
   inputfile = 'input'
   if len(sys.argv) == 2:
     inputfile = sys.argv[1]
-[32mProcessing: [33minput.3[39m...
-Traceback (most recent call last):
-  File "/Users/jonathan.lozinski/Development/advent_of_code/2024/template.py/puzzle.py", line 25, in <module>
-    inp = open(inputfile, 'r').read()
-FileNotFoundError: [Errno 2] No such file or directory: 'input.3'
+  print(Fore.GREEN + 'Processing:' + Fore.YELLOW + inputfile + Fore.RESET + '...')
+  inp = open(inputfile, 'r').read()
+  puz.process(inp)
+  puz.result()
