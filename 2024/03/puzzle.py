d68794513621b383678efe533ac52d354075e32f
Jonathan Lozinski <jonathan.lozinski@dayshape.com>
2024-12-03 06:50:07 +0000

    Day 3

---
+++ 2024/03/puzzle.py
@@ -0,0 +1,72 @@
+import sys
+from colorama import Fore
+import re
+
+class Puzzle:
+
+  def process(self, text):
+    self.instructions = []
+    for line in text.split('\n'):
+      self.process_line(line)
+
+  def process_line(self, line):
+    if line != '':
+      self.parse_instructions(line)
+  
+  def parse_instructions(self, line):
+    #p = re.compile(r'((mul|do|don\'t)\(((\d+),(\d+)){0,1}\))')
+    p = re.compile(r'((mul)\((\d+),(\d+)\)|(do)\(\)|(don\'t)\(\))')
+    matches = p.findall(line)
+    for match in matches:
+       if match[0].startswith('mul'):
+          inst = 'mul'
+          a = int(match[2])
+          b = int(match[3])
+       elif match[0].startswith('don'):
+          inst = "don't"
+          a = 0
+          b = 0
+       else:
+          inst = 'do'
+          a = 0
+          b = 0
+       self.instructions.append((inst, a, b))
+
+  def result(self):
+    total = 0
+    enabled = True
+    for i in self.instructions:
+       inst, a, b = i
+       if inst == 'mul':
+         if enabled:
+            m = a * b
+            print(i, m)
+            total += m
+         else:
+            print(i, m, '<disabled>')
+       elif inst == 'do':
+         print('<enable>')
+         enabled = True
+       else: #dont
+         print('<disable>')
+         enabled = False
+    print('Total:', total)
+
+  def result1(self):
+    total = 0
+    for i in self.instructions:
+       m = i[1] * i[2]
+       print(i, m)
+       total += m
+    print('Total:', total)
+
+
+if __name__ == '__main__':
+  puz = Puzzle()
+  inputfile = 'input'
+  if len(sys.argv) == 2:
+    inputfile = sys.argv[1]
+  print(Fore.GREEN + 'Processing:' + Fore.YELLOW + inputfile + Fore.RESET + '...')
+  inp = open(inputfile, 'r').read()
+  puz.process(inp)
+  puz.result()
---
+++ 2024/03/puzzle.test.py
@@ -0,0 +1,32 @@
+import unittest
+import puzzle as puz
+
+class TestPuzzle(unittest.TestCase):
+
+  def test_puzzle_extracts_the_instructions(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""mul(1,2)""")
+    self.assertEqual(puzzle.instructions[0], ('mul', 1, 2))
+
+  def test_puzzle_extracts_multiple_instructions(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""mul(1,2)mul(3,4)""")
+    self.assertEqual(puzzle.instructions[1], ('mul', 3,4))
+
+  def test_puzzle_extracts_only_valid(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""mul(1,2mul(3,4)""")
+    self.assertEqual(puzzle.instructions[0], ('mul', 3,4))
+
+  def test_puzzle_extracts_do_instructions(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""mul(1,2)do()mul(3,4)""")
+    self.assertEqual(puzzle.instructions[1], ('do', 0, 0))
+
+  def test_puzzle_extracts_dont_instructions(self):
+    puzzle = puz.Puzzle()
+    puzzle.process("""mul(1,2)don't()mul(3,4)""")
+    self.assertEqual(puzzle.instructions[1], ("don't", 0, 0))
+
+if __name__ == '__main__':
+    unittest.main()
